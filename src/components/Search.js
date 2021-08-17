0cd935e541f13241cfbcdbee89b939658f5ab5e5
neo <neomatrixtrilogy0@gmail.com>
2021-08-16 22:39:54 -0400

    started working on the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  const [user, setUser] = React.useState('')
+  const [user, setUser] = React.useState("");
+  const { requests, error } = React.useContext(GithubContext);
+
   //global context
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (user) {
-      
     }
     console.log(user);
-  }
+  };
   return (
     <section className="section">
-      <Wrapper className='section-center'>
+      <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
-            <input type="text" placeholder='enter github user' value={user} onChange={(e) => setUser(e.target.value)}/>
-            <button type="submit">Search</button>
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            {requests > 0 && <button type="submit">Search</button>}
           </div>
         </form>
-        <h3>requests: 60 / 60</h3>
+        <h3>requests: {requests} / 60</h3>
       </Wrapper>
     </section>
-  )
+  );
 };
 
 const Wrapper = styled.div`
